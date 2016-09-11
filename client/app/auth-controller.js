994f3a6942b0f2a9ca69e2e583d48295cc96dd21
flyinthesky <flyinthesky1610@gmail.com>
2016-09-11 09:48:25 +0700

    Add username to top bar

--- client/app/auth-controller.js
+++ client/app/auth-controller.js
@@ -1,8 +1,9 @@
 /*Auth Controller*/
 
-pdadmin.controller('AuthCtrl', ['$scope', 'Notification', 'User', '$rootScope', '$state',function($scope, Notification, User, $rootScope, $state) {
+pdadmin.controller('AuthCtrl', ['$scope', 'Notification', 'User', '$rootScope', '$state', '$window', function($scope, Notification, User, $rootScope, $state, $window) {
 
   $rootScope.is_authenticated = User.isAuthenticated();
+  $rootScope.username = '' || $window.localStorage.username;
 
   $scope.user = {
     email: '',
@@ -19,7 +20,9 @@ pdadmin.controller('AuthCtrl', ['$scope', 'Notification', 'User', '$rootScope', 
   $scope.login = function() {
     User.login($scope.user, function(data){
       $rootScope.is_authenticated = true;
-      Notification.success('Welcome to Peddler Admin!')
+      $rootScope.username = data.user.username;
+      $window.localStorage.username = data.user.username;
+      Notification.success('Welcome ' + $rootScope.username + ' to Peddler Admin!');
       $state.go('app');
     },
     function(err) {
